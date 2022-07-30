9a763cfb3f7f37cbe9372bee8ae284979c076cd2
Andrey Asabin <asabin01@mail.ru>
2022-07-30 13:31:45 +0300

    Full Math

--- js_2.js
+++ js_2.js
@@ -28,12 +28,41 @@
 // console.log(Number.parseFloat(stringFloat) + 2) // 42.42
 // console.log(+stringFloat + 2) // 42.42
 
-console.log(0.4 + 0.2)          //0.6000000000000001 float
-console.log(2/5 + 1/5)          //0.6000000000000001 float
-console.log((0.4 + 0.2).toFixed(4))  //str 0.6000
-console.log(parseFloat((0.4 + 0.2).toFixed(4)))  //float 0.6
+// console.log(0.4 + 0.2)          //0.6000000000000001 float
+// console.log(2/5 + 1/5)          //0.6000000000000001 float
+// console.log((0.4 + 0.2).toFixed(4))  //str 0.6000
+// console.log(parseFloat((0.4 + 0.2).toFixed(4)))  //float 0.6
 
-// BigInt
+// 2 BigInt
 
-console.log(typeof 900719925474099199999)   // number
-console.log(typeof 900719925474099199999n)  // bigint
+// console.log(typeof 900719925474099199999)   // number
+// console.log(typeof 900719925474099199999n)  // bigint
+// // BigInt работает только с BigInt
+// console.log(900719925474099199999n - 90071992547409919999n)  // bigint
+// // console.log(10n - 4) // error
+// // console.log(parseInt(10n) - 4) // 6
+// console.log(-900719925474099199999n)
+// // console.log(900719925474099199999.2323n) // must be integer
+// console.log(10n - BigInt(4)) // 6n
+// console.log(5n / 2n) // 2n отсекаем дробную часть
+
+// 3 Math
+console.log(Math.E)
+console.log(Math.PI)
+console.log(Math.sqrt(25)) // 5
+console.log(Math.pow(5, 3)) // 125
+console.log(25**(1/2)) // 5
+console.log(Math.abs(-3)) // 3
+console.log(Math.max(5, 3, 43)) // 43
+console.log(Math.min(5, 3, 43)) // 3
+console.log(Math.floor(4.9)) // 4 округление в меньшую сторону
+console.log(Math.ceil(4.9)) // 5 округление в большую сторону
+console.log(Math.round(4.9)) // 4 округление в ближайшую сторону
+console.log(Math.trunc(4.9)) // 4 отсекаем дробную часть
+console.log(Math.random())
+// 4 Example
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max-min+1) + min)
+}
+
+console.log(getRandom(10, 42))
