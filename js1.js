56cd6f1da7335f1e89bd82722ed1130c6580ee39
Andrey Asabin <asabin01@mail.ru>
2023-03-29 14:51:00 +0300

    js1D

--- js1.js
+++ js1.js
@@ -26,7 +26,7 @@ TODO: refactor this method so that it conforms to the API
 
 // mdn *Что я хочу спросить*
 //! Переменные
-// var firstName = 'Andrey';    // обычная переменная, устаревший метод 
+// var firstName = 'Andrey';    // обычная переменная, устаревший метод
 // const lastName = 'Asabin';    //* константа (нельзя изменить "Uncaught TypeError: Assignment to constant variable.")
 // let age = 26;                 //* обычная переменная, лучше это
 // const isProgrammer = true;    // обязательно с маленькой t
@@ -40,12 +40,12 @@ TODO: refactor this method so that it conforms to the API
 // const 5withNum = 5;       //* ошибка, переменные с цифрой в начале
 
 //! Мутирование
-//* конкатенация, вызывает переменную age с методом .toString() 
+//* конкатенация, вызывает переменную age с методом .toString()
 // console.log('Имя человека: ' + firstName + ', а возраст: ' + age)  // это строка
-// console.log(age)             
-// console.log(age.toString())  
-// alert('Имя человека: ' + firstName + ', а возраст: ' + age) 
-// const firstName = 'Andrey' 
+// console.log(age)
+// console.log(age.toString())
+// alert('Имя человека: ' + firstName + ', а возраст: ' + age)
+// const firstName = 'Andrey'
 // const lastName = prompt('введите фамилию')
 // alert(firstName + ' ' + lastName)
 
@@ -68,27 +68,26 @@ TODO: refactor this method so that it conforms to the API
 // console.log(a)       //* 11 (после инкремента, который выше)
 // console.log(++a)     // 12
 // console.log(--a)     // 11
+
 // let c = 32
-// c = c + a            // идентичны 
-// c += a               // идентичны 
+// c = c + a            // идентичны
+// c += a               // идентичны
 // console.log(с)
 
 
 //! Типы данных
-console.log()
-
-const isProgrammer = true    // boolean
-const name = 'Andrey'        // string
-const age = 20               // number
-let x                        // undefined
-                                // null
+// const isProgrammer = true;    // boolean - в консоли цвет фиолетовый
+// const firstName = 'Andrey';    // string - белый
+// const age = 20;   // number - фиолетовый
+// let x;                      // undefined - серый
+// null
 //const y // нельзя создать, ибо в константу нужно занести что-то по умолчанию
-// console.log(typeof isProgrammer)
-// console.log(typeof name)
-// console.log(typeof age)
-// console.log(typeof x)
-// console.log(typeof null)     //* object (неточность)
-// console.log(null)            // null
+// console.log(isProgrammer)
+// console.log(firstName)
+// console.log(age)
+// console.log(x)
+// console.log(typeof null)     //* object (неточность) - белый
+// console.log(null)            // null - серый
 
 //! Приоритет операторов
 //* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
@@ -96,10 +95,10 @@ let x                        // undefined
 // const birthYear = 2002
 // const currentYear = 2022
 //* сначала вычитание, потом сравнение, потом присваивание
-// const isFullAge = currentYear - birthYear >= fullAge 
+// const isFullAge = currentYear - birthYear >= fullAge
 // console.log(isFullAge)
 
-// 6 Условные операторы
+//! Условные операторы
 // const courseStatus = 'pending'
 // if (courseStatus === 'ready') {
 //     console.log('Курс готов')
@@ -115,34 +114,38 @@ let x                        // undefined
 // } else {
 //     console.log('Всё не готово')
 // }
-// // одинаково (Тернарное выражение)
+//* одинаково (Тернарное выражение)
 // isReady ? console.log('Всё готово') : console.log('Всё не готово')
 
-// // const num1 = 42 // number
+// const num1 = 42 // number
 // const num2 = '42' //string
 // console.log(num1 == num2) // true (JS приводит одному типу данных, к строке)
 // console.log(num1 === num2) // false
 
-// 7 Булевая логика
+//! Булевая логика
 // (3 года универа с этим)
+//* https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Expressions_and_Operators#%D0%BB%D0%BE%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5_%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D1%8B
 
-// 8 Функции
-// function calcAge(year){
-//     return 2022 - year
-// }
-// // const myAge = calcAge(2001)
-// // console.log(calcAge(2001))
-// function logInfoAbout(name, year) {
-//     const age = calcAge(year)
+//! Функции
+/* function calcAge(year){
+    return 2022 - year
+}
+const currYear = 2001
+console.log(currYear)
+console.log(calcAge(currYear))
+let myAge = calcAge(currYear)
+console.log(myAge)
 
-//     console.log('Имя: ' + name + ' Возраст: ' + age)
-// }
-// logInfoAbout('Andrey', 2001)
+function logInfoAbout(name, year) {
+    const age = calcAge(year)
+    console.log('Имя: ' + name + ' Возраст: ' + age)
+}
+logInfoAbout('Andrey', 2001) */
 
-// 9 Массивы
+//! Массивы
 
-// // const cars = new Array ('Lada','BMW','Renault')
-// const cars = ['Lada','BMW','Renault'] // упрощено 
+// const cars = new Array ('Lada','BMW','Renault')
+// const cars = ['Lada','BMW','Renault'] // упрощено
 // console.log(cars) // (3) ['Lada', 'BMW', 'Renault']
 // console.log(cars[0]) // Lada
 // console.log(cars.length) // 3
@@ -152,34 +155,34 @@ let x                        // undefined
 // cars[cars.length] = 'Ford' // добавим
 // console.log(cars) // (5) ['Porshe', 'BMW', 'Renault', 'Mazda', 'Ford']
 
-// 10 Циклы
-// const cars = ['Lada','BMW','Renault'] 
+//! Циклы
 
-// // for (let i = 0; i < cars.length; i++){
-// //     const car = cars[i]
-// //     console.log(car)
-// // }
+/* const cars = ['Lada','BMW','Renault']
+for (let i = 0; i < cars.length; i++){
+     const car = cars[i]
+     console.log(car)
+}
 
-// for (let car of cars){
-//     console.log(car)
-// }
+for (let car of cars){
+    console.log(car)
+} */
 
-// // 11 Объекты
-// const person = {
-//     firstName: 'Andrey',
-//     lastName: 'Asabin',
-//     year: 2001,
-//     languages: ['Ru', 'Eng', 'Chi'],
-//     hasWife: false,
-//     greet: function(){
-//         console.log('greet')
-//     }
-// }
-// console.log(person) // вывод всех полей
-// console.log(person.firstName) // вывод одного поля
-// console.log(person['lastName']) // вывод одного поля
-// person.hasWife = true // изменение
-// person.greet() // вызов метода у объекта
+//! Объекты
+/* const person = {
+    firstName: 'Andrey',
+    lastName: 'Asabin',
+    year: 2001,
+    languages: ['Ru', 'Eng', 'Chi'],
+    hasWife: false,
+    greet: function(){
+        console.log('qq')
+    }
+}
+console.log(person) // вывод всех полей
+console.log(person.firstName) // вывод одного поля
+console.log(person['lastName']) // вывод одного поля
+person.hasWife = true // изменение
+person.greet() // вызов метода у объекта */
 
 
 
